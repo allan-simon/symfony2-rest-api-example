a7d68e3b3decb480fac4660258a20eb4e4fd3b1f
Allan Simon <simona@gobeta.com.cn>
2015-05-24 20:53:25 +0800

    activated the JMSSerializer bundle

--- MyApplication/app/AppKernel.php
+++ MyApplication/app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
             new AppBundle\AppBundle(),
         );
 
