09ce5f46826d33806f5ed190f01b7eab6dea84a3
Allan Simon <simona@gobeta.com.cn>
2015-05-24 17:24:15 +0800

    added more comments in the code

--- MyApplication/src/AppBundle/Controller/ArticlesController.php
+++ MyApplication/src/AppBundle/Controller/ArticlesController.php
@@ -2,16 +2,20 @@
 
 namespace AppBundle\Controller;
 
-use FOS\RestBundle\Controller\FOSRestController;
-
-class ArticlesController extends FOSRestController
+class ArticlesController
 {
 
+    /**
+     * Note: here the name is important
+     * get => the action is restricted to GET HTTP method
+     * Article => (without s) generate /articles/SOMETHING
+     * Action => standard things for symfony for a controller method which
+     *           generate an output
+     *
+     * it generates so the route GET .../articles/{id}
+     */
     public function getArticleAction($id)
     {
         return array('hello' => 'world');
     }
-
 }
-
-
